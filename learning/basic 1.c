4d29189a0f33eff01435cf3d906ec8922aae0051
rahulsaha1045451444 <monasaha1045451444@gmail.com>
2024-06-11 21:35:49 +0530

    main

--- learning/basic 1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int age =39;//age is a integer so that here is int before age//
-    printf("the age is %d\n",age);//the age is showing and the "%d" is used for integer and the "age" is targeted to the age//
-        return 0;//return 0;=end the  code//
-        }
--- learning/scan-function 1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    int age;
-    printf("Enter age");
-    scanf("%d", &age);
-    printf("birth day :%d",2024-age);
-        return 0;
-        }
