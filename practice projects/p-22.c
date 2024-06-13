890cd3f50df13542c8081069eefc7971670f3377
rahulsaha1045451444 <monasaha1045451444@gmail.com>
2024-06-13 22:47:32 +0530

    p-22

---
+++ practice projects/p-22.c
@@ -0,0 +1,14 @@
+/* Print reverse of the table for a number n */
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int n;
+    printf("enter n : ");
+    scanf("%d", &n);
+    for (int i = 10; i >= 1; i--)
+    {
+        printf("%d*%d=%d\n", n, i, n * i);
+    }
+    return 0;
+}
